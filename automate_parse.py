65832e6532043a434b0eca8eed7c7c77b7364b7c
Md. Shamim Rahman <apubdtea@gmail.com>
2024-01-01 21:10:47 +0600

    refactor: add main function
    Added main function to automate_parse.py file to follow convention.

--- automate_parse.py
+++ automate_parse.py
@@ -1,9 +1,15 @@
 import os
 
-os.chdir('./files_demo')
 
-for f in os.listdir():
-  if os.path.isfile(f):
-    f_name, f_ext = os.path.splitext(f)
-    new_name = f_name.replace('.', '-') + f_ext
-    os.rename(f, new_name)
+def main():
+    os.chdir("./files_demo")
+
+    for f in os.listdir():
+        if os.path.isfile(f):
+            f_name, f_ext = os.path.splitext(f)
+            new_name = f_name.replace(".", "-") + f_ext
+            os.rename(f, new_name)
+
+
+if __name__ == "__main__":
+    main()
