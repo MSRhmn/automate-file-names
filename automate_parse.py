700eebe62426bb0b2890f097586743d130fe3d22
Md. Shamim Rahman <apubdtea@gmail.com>
2024-01-06 17:01:34 +0600

    docs: correct spelling of comments

--- automate_parse.py
+++ automate_parse.py
@@ -1,6 +1,6 @@
 import os
 
-print("*** Please make sure, you are passing the correct path ****")
+print("*** Please make sure that you are passing the correct path ***")
 wd_path = input("Enter a working directory path: ")
 
 
@@ -19,11 +19,11 @@ def main():
 
     check_path()
 
-    # This listdir() method from os module checks files in the current working directory.
+    # This listdir method from os module checks files/directories in the current working directory.
     for f in os.listdir():
-        # isfile() method checks if it is a file or not.
+        # isfile method checks if it is a file or not.
         if os.path.isfile(f):
-            # This splitext() does as it says, splits the extension from file name.
+            # This splitext method does as it says, splits the extension from file name.
             f_name, f_ext = os.path.splitext(f)
             new_name = f_name.replace(".", "-") + f_ext
             os.rename(f, new_name)
