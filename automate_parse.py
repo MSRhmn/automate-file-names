b82d62c43df7284b4bee58c97ec1637b73cc3cf4
Md. Shamim Rahman <apubdtea@gmail.com>
2024-01-06 17:32:47 +0600

    feat: allow user to send file modifying inputs
    Allows user to pick whatever information they are trying to replace by
    adding mark_item and change_item input variables.

--- automate_parse.py
+++ automate_parse.py
@@ -1,7 +1,10 @@
 import os
 
-print("*** Please make sure that you are passing the correct path ***")
+print("*** Make sure that you are passing the correct path ***\n")
+
 wd_path = input("Enter a working directory path: ")
+mark_item = input("Enter the item name that has to be replaced: ")
+change_item = input("Enter the item name that will be replaced with: ")
 
 
 def check_path():
@@ -25,7 +28,11 @@ def main():
         if os.path.isfile(f):
             # This splitext method does as it says, splits the extension from file name.
             f_name, f_ext = os.path.splitext(f)
-            new_name = f_name.replace(".", "-") + f_ext
+            # Check if the user has provided file modifying inputs.
+            if mark_item and change_item:
+                new_name = f_name.replace(mark_item, change_item) + f_ext
+            else:
+                new_name = f_name.replace(".", "-") + f_ext
             os.rename(f, new_name)
 
 
