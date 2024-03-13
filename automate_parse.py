37475766a7b7553e62ae0f1057f2c8a1a0c38a38
Md. Shamim Rahman <apubdtea@gmail.com>
2024-03-13 15:50:03 +0600

    feat: implement object-oriented approach in app

--- automate_parse.py
+++ automate_parse.py
@@ -1,36 +1,45 @@
 from pathlib import Path
 
-print("** Make sure that you are passing the correct path **\n")
 
-path = input("Enter a working directory path: ")
-mark_char = input("Enter the char name that has to be replaced: ")
-change_char = input("Enter the char name that will be replaced with: ")
+class FileRenamer:
+    """A simple way to design file renaming class"""
 
+    def __init__(self, path, mark_char, change_char):
+        self.working_dir = self.check_path(path)
+        self.mark_char = mark_char
+        self.change_char = change_char
 
-def check_path():
-    """
-    This function checks if the user has passed a path.
-    Otherwise it will use the default demo path.
-    """
-    if path:
-        return Path(path).resolve()
-    else:
-        return Path("./files_demo").resolve()
+    def check_path(self, path):
+        """
+        This function checks if the user has passed a path.
+        Otherwise it will use the default demo path.
+        """
+        if path:
+            return Path(path).resolve()
+        else:
+            return Path("./files_demo").resolve()
+
+    def rename_files(self):
+        """Rename files in the working directory."""
+        # This iterdir method from pathlib module checks files/directories in the current working directory.
+        for f in self.working_dir.iterdir():
+            # isfile method checks if it is a file or not.
+            if f.is_file():
+                # f.stem gets the file name while f.suffix has extension name.
+                f_name, f_ext = f.stem, f.suffix
+                if self.mark_char and self.change_char:
+                    new_name = f_name.replace(self.mark_char, self.change_char) + f_ext
+                f.rename(self.working_dir / new_name)
 
 
 def main():
+    print("** Make sure that you are passing the correct path **\n")
+    path = input("Enter a working directory path: ")
+    mark_char = input("Enter the char name that has to be replaced: ")
+    change_char = input("Enter the char name that will be replaced with: ")
 
-    working_dir = check_path()
-
-    # This iterdir method from pathlib module checks files/directories in the current working directory.
-    for f in working_dir.iterdir():
-        # isfile method checks if it is a file or not.
-        if f.is_file():
-            # f.stem gets the file name while f.suffix has extension name.
-            f_name, f_ext = f.stem, f.suffix
-            if mark_char and change_char:
-                new_name = f_name.replace(mark_char, change_char) + f_ext
-            f.rename(working_dir / new_name)
+    renamer = FileRenamer(path, mark_char, change_char)
+    renamer.rename_files()
 
 
 if __name__ == "__main__":
