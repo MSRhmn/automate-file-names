55c5c858711396885c084c72ae67753013136943
Md. Shamim Rahman <apubdtea@gmail.com>
2024-03-13 17:32:38 +0600

    feat: add 'print_input' function
    Adding printing function to print the user that which directory will be
    using before applying renaming operation on the files.

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from automate_parse import FileRenamer
 
 
+def print_input():
+    """Checking if the user has provided a path or not."""
+    if path:
+        print(
+            f"\n>> File renaming operation will be held on '{path}' this path on your machine. <<\n"
+        )
+    else:
+        print(
+            f"\n>> File renaming operation will be held on 'files_demo' directory. <<\n"
+        )
+
+
 def main():
-    print("** Make sure that you are passing the correct path **\n")
+    print("** Make sure that you are passing the correct path. **\n")
     path = input("Enter a working directory path: ")
+    print_input()
     mark_char = input("Enter the char name that has to be replaced: ")
     change_char = input("Enter the char name that will be replaced with: ")
 
